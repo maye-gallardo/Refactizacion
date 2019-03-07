19d1f04231ad32de870063135df33803c0acaa05
MayeGer <maye_gallardo@hotmail.com>
2019-03-06 23:06:42 -0400

    eighth refactoring

--- statement.js
+++ statement.js
@@ -2,10 +2,10 @@ function statement (invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-  function format(aNumber) {
+  function usd(aNumber) {
     return new Intl.NumberFormat("en-US",
                 { style: "currency", currency: "USD",
-                  minimumFractionDigits: 2 }).format(aNumber);
+                  minimumFractionDigits: 2 }).format(aNumber/100);
   }
   
   for (let perf of invoice.performances) {
@@ -14,11 +14,13 @@ function statement (invoice, plays) {
     volumeCredits += volumeCreditsFor(perf);
 
     // print line for this order
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `Amount owed is ${format(totalAmount/100)}\n`;
+
+  
+  result += `Amount owed is ${usd(totalAmount)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
 
