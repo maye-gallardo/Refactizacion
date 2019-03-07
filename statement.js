805d3eb5ecff9e4c0f37a7b84fe145d299a77df4
MayeGer <maye_gallardo@hotmail.com>
2019-03-06 21:50:05 -0400

    fourth  refactoring

--- statement.js
+++ statement.js
@@ -6,23 +6,27 @@ function statement (invoice, plays) {
                       { style: "currency", currency: "USD",
                         minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(play, perf);
+    const play = playFor(perf);
+    let thisAmount = amountFor(playFor(perf), perf);
 
     // add volume credits
     volumeCredits += Math.max(perf.audience - 30, 0);
     // add extra credit for every ten comedy attendees
-    if ("comedy" === play.type)
+    if ("comedy" === playFor(perf).type)
       volumeCredits += Math.floor(perf.audience / 5);
 
     // print line for this order
-    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+    result += ` ${playFor(perf).name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
   }
 
   result += `Amount owed is ${format(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
   return result;
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+   }
 
   function amountFor(play, aPerformance) {
     let result = 0;
